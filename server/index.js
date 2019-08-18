9d371bf34eb5eb6188c928c14b73fd2a9916a5dd
Ben <bensims89@live.com>
2019-08-18 15:09:58 -0700

    Finish

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const server = require('./server');
 
-server.listen(3000, function() {
-  console.log('Server is listening on http://localhost:3000');
-});
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
