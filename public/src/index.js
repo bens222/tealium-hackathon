99f4f11e136f66234c2272c7119607d5420cd93c
Ben <bensims89@live.com>
2019-08-18 13:09:12 -0700

    Added logic for badges

--- public/src/index.js
+++ public/src/index.js
@@ -2,26 +2,33 @@ $(document).ready(function() {
   $.getJSON('./src/data.json', function(vistorData) {
     const singleUser = vistorData.splice(0, 1);
     const singleUserAudience = singleUser[0].audiences.sort();
-    const singleUserBadge = singleUser.badges;
+    const singleUserBadge = singleUser[0].badges.sort();
     
+    // Audience logic
     let visitorAudiences = [];
     for(let i = 0; i < vistorData.length; i++) {
       visitorAudiences.push({index: i, value: vistorData[i].audiences.sort()})
     }
-    let capturedResult = [];
+    let capturedAudiences = [];
     for(let j = 0; j < visitorAudiences.length; j++) {
       if(singleUserAudience.join() === visitorAudiences[j].value.join()) {
-        capturedResult.push(visitorAudiences[j])
+        capturedAudiences.push(visitorAudiences[j])
       }
     }
-    console.log('label:: ', capturedResult);
-    
-    // singleUserAudience.forEach((cur, i, arr) => {
-    //   vistorData.forEach((current, index, array) => {
-    //       if (cur[i] === current[index]) {
-    //           string.splice(i, 1, unique[index])
-    //       }
-    //   })
-    // })    
+    console.log('label:: ', capturedAudiences);
+
+    // badge logic
+    let visitorBadges = [];
+    for(let i = 0; i < vistorData.length; i++) {
+      visitorBadges.push({index: i, value: vistorData[i].badges.sort()})
+    }
+    let capturedBadges = [];
+    for(let j = 0; j < visitorBadges.length; j++) {
+      if(singleUserBadge.join() === visitorBadges[j].value.join()) {
+        capturedBadges.push(visitorBadges[j])
+      }
+    }
+    console.log('label:: ', capturedBadges);
+   
   })
 })
